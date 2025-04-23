a2649d8541f1a682e90b39fc12ee5a2950bf1458
Abhii-bhardwaj <abhibhardwaj622@gmail.com>
2025-04-23 14:55:12 +0530

    updated socket logic for frontend

--- Frontend/src/hooks/useSocket.hook.js
+++ Frontend/src/hooks/useSocket.hook.js
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import { useAuthStore } from "../store/useAuthStore";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5001";
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
 export const useSocket = (initializeListeners = () => {}) => {
   const [socket, setSocket] = useState(null);
